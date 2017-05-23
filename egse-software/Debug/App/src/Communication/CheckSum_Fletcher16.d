42b4ff453bee39bee1ce50c3f1b9d988e9a9d422
Piotr Kuligowski <kontakt@piotrkuligowski.pl>
2017-05-23 12:38:10 +0200

    post-build rubbish removed

--- egse-software/Debug/App/src/Communication/CheckSum_Fletcher16.d
+++
@@ -1,6 +0,0 @@
-App/src/Communication/CheckSum_Fletcher16.d \
- App/src/Communication/CheckSum_Fletcher16.o: \
- ../App/src/Communication/CheckSum_Fletcher16.cpp \
- ../App/src/Communication/CheckSum_Fletcher16.h
-
-../App/src/Communication/CheckSum_Fletcher16.h:
